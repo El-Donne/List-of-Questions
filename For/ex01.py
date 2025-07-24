afc1d27299ce2afb20d20db6188c3a7602daa075
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-07-24 13:31:21 -0300

    For loop

---
+++ For/ex01.py
@@ -0,0 +1,5 @@
+fruits = ['apple', 'banana', 'coconut']
+colors = ['red', 'yellow', 'brown']
+for x in fruits:
+    for y in colors: # Para cada item percorrido dentro de "fruits" ele percorre cada item de "colors"
+        print(x, y) # Imprime uma sequência
